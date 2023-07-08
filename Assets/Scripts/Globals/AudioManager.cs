f8730ef8b52062b53ec47372325a8c0cec38c5a8
Ethan Morrill-Ploum <emorrillploum@heroforge.com>
2023-07-08 11:41:36 -0400

    Audio manager

---
+++ Assets/Scripts/Globals/AudioManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    
+    [HideInInspector] public FMOD.Studio.EventInstance musicInstance;
+
+    private void Awake() {
+        if (!instance) {
+            instance = this;
+        } else {
+            GameObject.DestroyImmediate(this.gameObject);
+        }
+        GameObject.DontDestroyOnLoad(this);
+    }
+
+    private void Start() {
+        PlayMusic(GlobalVariables.MUSIC_MAIN_MENU);
+    }
+
+    public void PlayOneShot(string eventName, GameObject obj) {
+        FMOD.Studio.EventInstance eventInstance = FMODUnity.RuntimeManager.CreateInstance(eventName);
+        if (obj != null) {
+            eventInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(obj));
+        }
+        eventInstance.start();
+        eventInstance.release();
+    }
+
+    public static void PlayOneShot(string eventName) {
+        AudioManager.instance.PlayOneShot(eventName, null);
+    }
+
+    public static void PlayMusic(string eventName) {
+        if (AudioManager.IsPlaying(AudioManager.instance.musicInstance)) {
+            Debug.LogWarning("Music is already playing");
+            return;
+        }
+
+        AudioManager.instance.musicInstance = FMODUnity.RuntimeManager.CreateInstance(eventName);
+        AudioManager.instance.musicInstance.start();
+    }
+
+    public static void StopMusic() {
+        if (!AudioManager.IsPlaying(AudioManager.instance.musicInstance)) {
+            Debug.LogWarning("Music is not playing");
+            return;
+        }
+
+        AudioManager.instance.musicInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        AudioManager.instance.musicInstance.release();
+    }
+
+    public static bool IsPlaying(FMOD.Studio.EventInstance eventInstance) {
+	FMOD.Studio.PLAYBACK_STATE state;   
+	eventInstance.getPlaybackState(out state);
+	return state != FMOD.Studio.PLAYBACK_STATE.STOPPED;
+}
+}
---
+++ Assets/Scripts/Globals/GlobalVariables.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables : MonoBehaviour
+{
+    public static string MUSIC_MAIN_MENU = "event:/Music/main_theme";
+}
