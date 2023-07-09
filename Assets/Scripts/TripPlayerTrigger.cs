fd25a1a31545dc2d628f3013844079cd0bce4b4f
Jeremy Helsel <JIHlucky7@gmail.com>
2023-07-09 08:29:21 -0500

    Destroy stick on trip

--- Assets/Scripts/TripPlayerTrigger.cs
+++ Assets/Scripts/TripPlayerTrigger.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class TripPlayerTrigger : MonoBehaviour 
 {
     public LayerMask playerLayer;
-    
+    [SerializeField]GameObject parent = null;
+
+
     private void OnTriggerEnter(Collider other) {
         if (playerLayer == (playerLayer | (1 << other.gameObject.layer))) {
             CharacterController characterController = other.transform.root.GetComponent<CharacterController>();
             if (characterController != null) {
                 characterController.TriggerFrenzy();
+                if(parent != null) {
+                    Destroy(parent);
+                }
             }
         }
     }
