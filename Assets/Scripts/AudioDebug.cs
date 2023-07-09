e5b60c0ba170dabb6c65fabb2cad8091d392f619
Jeremy Helsel <JIHlucky7@gmail.com>
2023-07-08 21:00:24 -0500

    Further updated audio banks.

--- Assets/Scripts/AudioDebug.cs
+++ Assets/Scripts/AudioDebug.cs
@@ -7,16 +7,16 @@ public class AudioDebug : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.H)) {
-            AudioManager.PlayOneShot(GlobalVariables.SFX_OWNER_STOP);
+            AudioManager.PlayOneShot(GlobalVariables.SFX_DOG_SNIFF);
         }
         if(Input.GetKeyDown(KeyCode.J)) {
-            AudioManager.PlayOneShot(GlobalVariables.SFX_STEP_CONCRETE);
+            AudioManager.PlayOneShot(GlobalVariables.SFX_DOG_BARK);
         }
         if(Input.GetKeyDown(KeyCode.K)) {
-            AudioManager.PlayOneShot(GlobalVariables.SFX_STEP_GRASS);
+            AudioManager.PlayOneShot(GlobalVariables.SFX_DOG_WALK_CYCLE);
         }
         if(Input.GetKeyDown(KeyCode.L)) {
-            AudioManager.PlayOneShot(GlobalVariables.SFX_OWNER_FEEDBACK);
+            AudioManager.PlayOneShot(GlobalVariables.SFX_OWNER_NO);
         }
     }
 }
--- Assets/Scripts/Globals/GlobalVariables.cs
+++ Assets/Scripts/Globals/GlobalVariables.cs
@@ -6,8 +6,19 @@ public class GlobalVariables : MonoBehaviour
 {
     public static string MUSIC_MAIN_MENU = "event:/Music/main_theme";
 
+    public static string SOUNDSCAPE_PARK_AMBIENCE = "event:/soundscape/ambience";
+
+    public static string SFX_DOG_BARK = "event:/sound effects/dog_bark";
+    public static string SFX_DOG_SNIFF = "event:/sound effects/sniff";
+    public static string SFX_DOG_WALK_CYCLE = "event:/sound effects/walk_cycle_dog";
+    public static string SFX_FIRE_HYDRANT = "event:/sound effects/fire_hydrant";
     public static string SFX_STEP_CONCRETE = "event:/sound effects/owner_steps_concrete";
     public static string SFX_STEP_GRASS = "event:/sound effects/owner_steps_grass";
     public static string SFX_OWNER_FEEDBACK = "event:/sound effects/owner_feedback";
+    public static string SFX_OWNER_HEY = "event:/sound effects/owner_hey";
     public static string SFX_OWNER_STOP = "event:/sound effects/owner_stop";
+    public static string SFX_OWNER_NO = "event:/sound effects/owner_no";
+    public static string SFX_LEASH_PULL = "event:/sound effects/leash_pull";
+    public static string SFX_OWNER_BREATHING = "event:/sound effects/owner_breathing";
+    public static string SFX_UI_CLICK = "event:/ui_sfx/ui_click";
 }
